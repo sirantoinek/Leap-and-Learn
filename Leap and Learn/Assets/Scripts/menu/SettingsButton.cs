05a0dc045bdf6de4c11fdac1f529770689a475c4
df-ufgit <116981568+df-ufgit@users.noreply.github.com>
2024-11-19 17:11:41 -0500

    Leaderboard, login, and Player Data
    player data has Hats, Skins, and Coins

--- Leap and Learn/Assets/Scripts/menu/SettingsButton.cs
+++ Leap and Learn/Assets/Scripts/menu/SettingsButton.cs
@@ -5,6 +5,7 @@ public class SettingsButton : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
         // Load settings scene
     }
 
---
+++ Leap and Learn/Assets/Scripts/playfab/LeaderboardManager.cs
@@ -0,0 +1,41 @@
+using PlayFab.ClientModels;
+using PlayFab;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using System.Linq;
+using System.Collections;
+
+public class LeaderboardManager : MonoBehaviour
+{
+    private List<PlayerLeaderboardEntry> leaderboardEntries;
+
+    private void Start()
+    {
+        StartCoroutine(GetLeaderboardCoroutine());
+    }
+
+    private IEnumerator GetLeaderboardCoroutine()
+    {
+        while (true)
+        {
+            GetLeaderboard();
+            yield return new WaitForSeconds(5); // Poll Leaderboard every 5sec
+        }
+    }
+
+    public void GetLeaderboard()
+    {
+        leaderboardEntries = PlayFabController.Instance.GetLeaderboardEntries();
+
+        // Do something with leaderboardEntries
+        // if user is not in top 10 .. user will be the 11th entry (the last)
+        // 11th entry not operational yet....
+
+        foreach (var item in leaderboardEntries)
+        {
+            Debug.Log(string.Format("PLACE: {0} | NAME: {1} | HIGHSCORE: {2}",
+                item.Position, item.DisplayName, item.StatValue));
+        }
+    }
+}
